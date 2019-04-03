67501e0fb4077108dc0e756e487e98801fad8dba
Joseph Monreal <joemonreal23@icloud.com>
2019-04-02 23:32:48 -0700

    Updated links to link to correct page.

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
     <body>
         <h1>Select Quiz To Take!</h1>
         <br><br><a href="lhtlquiz.php">Learn How 2 Learn Quiz</a>
-        <br><br><a href="java.php">Java Quiz!</a>
-        <br><br><a href="php_.php">PHP Quiz!</a>
-        <br><br><a href="mysql.php">MySQL Quiz!</a>
+        <br><br><a href="javaquiz.php">Java Quiz!</a>
+        <br><br><a href="phpquiz.php">PHP Quiz!</a>
+        <br><br><a href="mysqlquiz.php">MySQL Quiz!</a>
         <br><br><a href="earth.php">Earth Quiz!</a>
         <br><br><a href="espanol.php">Espanol Quiz!</a>
     </body>    
